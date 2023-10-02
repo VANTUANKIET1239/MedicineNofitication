0465cf002e844a5c2ff99f6daedf8f5fe5d17bbf
Gwenz <89728007+nhuyne@users.noreply.github.com>
2023-10-02 19:22:55 +0700

    m1

--- src/app/slideshow/slideshow.module.ts
+++ src/app/slideshow/slideshow.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -15,6 +15,8 @@ import { SlideshowPage } from './slideshow.page';
     IonicModule,
     SlideshowPageRoutingModule
   ],
-  declarations: [SlideshowPage]
+  declarations: [SlideshowPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
 })
 export class SlideshowPageModule {}
--- src/app/slideshow/slideshow.page.ts
+++ src/app/slideshow/slideshow.page.ts
@@ -1,10 +1,8 @@
-import { AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NavigationExtras, Router } from '@angular/router';
-import { Prescription } from '../models/prescription';
-import { from } from 'rxjs';
-import { LoadingController, NavController } from '@ionic/angular';
-import { ComponentBase } from '../shared/ComponentBase/ComponentBase';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {register} from 'swiper/element/bundle';
+import { Swiper } from 'swiper/types';
+
+register();
 
 @Component({
   selector: 'app-slideshow',
@@ -12,10 +10,35 @@ import { ComponentBase } from '../shared/ComponentBase/ComponentBase';
   styleUrls: ['./slideshow.page.scss'],
 })
 export class SlideshowPage implements OnInit {
+  @ViewChild('swiper')
+  swiperRef:ElementRef|undefined;
+  swiper?=Swiper;
+
+  News = [
+    {id: 1, img: 'https://i.ytimg.com/vi/uS-j17YNKrM/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAz-bKzEMJTAN_EX5DHW1WhsIlGTg', title: 'Indian', detail:'https://thanhnien.vn/suc-khoe.htm'},
+    {id: 2, img: 'https://i.ytimg.com/vi/uS-j17YNKrM/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAz-bKzEMJTAN_EX5DHW1WhsIlGTg', title: 'Italian', detail:'https://thanhnien.vn/suc-khoe.htm'},
+    {id: 8, img: 'https://i.ytimg.com/vi/uS-j17YNKrM/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAz-bKzEMJTAN_EX5DHW1WhsIlGTg', title: 'Rolls', detail:'https://thanhnien.vn/suc-khoe.htm'},
+    {id: 7, img: 'https://i.ytimg.com/vi/uS-j17YNKrM/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAz-bKzEMJTAN_EX5DHW1WhsIlGTg', title: 'Burgers', detail:'https://thanhnien.vn/suc-khoe.htm'},
+    {id: 3, img: 'https://i.ytimg.com/vi/uS-j17YNKrM/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAz-bKzEMJTAN_EX5DHW1WhsIlGTg', title: 'Mexican', detail:'https://thanhnien.vn/suc-khoe.htm'},
+    {id: 4, img: 'https://i.ytimg.com/vi/uS-j17YNKrM/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAz-bKzEMJTAN_EX5DHW1WhsIlGTg', title: 'American', detail:'https://thanhnien.vn/suc-khoe.htm'},
+    {id: 5, img: 'https://i.ytimg.com/vi/uS-j17YNKrM/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAz-bKzEMJTAN_EX5DHW1WhsIlGTg', title: 'Chinese', detail:'https://thanhnien.vn/suc-khoe.htm'},
+    {id: 6, img: 'https://i.ytimg.com/vi/uS-j17YNKrM/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAz-bKzEMJTAN_EX5DHW1WhsIlGTg', title: 'Sea Food', detail:'https://thanhnien.vn/suc-khoe.htm'},
+  ];
+  
+  
 
   constructor() { }
 
+  swiperReady() {
+    this.swiper=this.swiperRef?.nativeElement.swiper;
+  }
+
+
+  swiperSlideChanged(e:any){
+    console.log('change: ',e);
+  }
   ngOnInit() {
+    
   }
 
 }
