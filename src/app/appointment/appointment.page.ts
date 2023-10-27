decc3e21de7be32e16b8ff637bf84403bc3e2a3e
Hungngyxn <100173529+Hungngyxn@users.noreply.github.com>
2023-10-27 20:11:51 +0700

    Update appointment.page.ts

--- src/app/appointment/appointment.page.ts
+++ src/app/appointment/appointment.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActionSheetController, ModalController, ToastController } from '@ionic/angular';
 import { AddAppointmentPage } from '../add-appointment/add-appointment.page';
@@ -6,6 +6,7 @@ import { EditAppointmentPage } from '../edit-appointment/edit-appointment.page';
 import { Appointment } from '../models/Appointment';
 import { GetdataService } from '../Services/addApointment/addApointment.service';
 import { LocalNotificationsService } from '../Services/addApointment/notifications.service';
+import { Auth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-appointment',
@@ -16,6 +17,8 @@ import { LocalNotificationsService } from '../Services/addApointment/notificatio
 export class Tab1Page {
   appointments: Appointment[] = [];
   id: any[] = [];
+  private auth:Auth = inject(Auth);
+  user$ =this.auth.currentUser;
 
   constructor(
     private modalCtrl: ModalController,
@@ -38,9 +41,23 @@ export class Tab1Page {
   }
 
   getdataAndScheduleNotifications() {
-    this.config.getListApoint('1', this.appointments, this.id).then(() => {
+    this.config.getListApoint(this.user$?.uid, this.appointments, this.id).then(() => {
       this.scheduleNotificationsForAppointments(this.appointments);
     });
+  }
+  scheduleNotificationsForAppointments(appointments: any[]) {
+    appointments.forEach((appointment: any) => {
+      const appointmentDate = new Date(appointment.dateTime);
+      const notificationTime = new Date(appointmentDate);
+      notificationTime.setHours(notificationTime.getHours() - 2); // Đặt thông báo 2 giờ trước cách lịch hẹn
+  
+      this.localNotificationsService.scheduleNotification(
+        'Thông báo lịch khám',
+        'Lịch khám lúc'+ appointment.date+ 'sắp tới',
+        appointment.id, // ID của thông báo (có thể sử dụng ID của lịch hẹn)
+        notificationTime.getTime()
+      );
+    }); 
   }
 
   formatDateTime(inputDateTime: Date) {
@@ -51,7 +68,6 @@ export class Tab1Page {
     const year = date.getFullYear();
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
-    
     if (date < currentDate) {
       return `${day}/${month}/${year} ${hours}:${minutes}` + '(Hết hạn)'; // quá hạn sẽ không thể sửa
     } else {
@@ -125,8 +141,7 @@ export class Tab1Page {
             this.config.deleteApoint(appointment);
             console.log('Tài liệu đã được xóa thành công.');
             this.presentToast('Xóa thành công');
-            this.getdataAndScheduleNotifications();
-                
+            this.getdataAndScheduleNotifications();               
           },
         },
         {
@@ -149,18 +164,5 @@ export class Tab1Page {
     });
     toast.present();
   }
-  scheduleNotificationsForAppointments(appointments: any[]) {
-    appointments.forEach((appointment: any) => {
-      const appointmentDate = new Date(appointment.dateTime);
-      const notificationTime = new Date(appointmentDate);
-      notificationTime.setHours(notificationTime.getHours() - 2); // Đặt thông báo 2 giờ trước cách lịch hẹn
   
-      this.localNotificationsService.scheduleNotification(
-        'Thông báo lịch khám',
-        'Lịch khám lúc'+ appointment.date+ 'sắp tới',
-        appointment.id, // ID của thông báo (có thể sử dụng ID của lịch hẹn)
-        notificationTime.getTime()
-      );
-    });
-  }
 }
